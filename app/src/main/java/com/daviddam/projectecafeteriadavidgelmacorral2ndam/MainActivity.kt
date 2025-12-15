357359f3fda28eed4ef67a0a74fc526b860671de
Dage10 <davidgelmacorral@gmail.com>
2025-12-15 20:03:24 +0100

    Implementar Bottom Navigation

--- app/src/main/java/com/daviddam/projectecafeteriadavidgelmacorral2ndam/MainActivity.kt
+++ app/src/main/java/com/daviddam/projectecafeteriadavidgelmacorral2ndam/MainActivity.kt
@@ -5,10 +5,14 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import sharedPreference.SharedPreference
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         Thread.sleep(3000)
         installSplashScreen()
@@ -20,5 +24,12 @@ class MainActivity : AppCompatActivity() {
         }
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNav.setupWithNavController(navController)
+
     }
 }
