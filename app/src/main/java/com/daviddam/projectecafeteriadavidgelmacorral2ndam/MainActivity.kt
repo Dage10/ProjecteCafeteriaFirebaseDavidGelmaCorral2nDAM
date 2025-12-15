71274c1e74dd70b71bb116e962518ad4df3e328d
Dage10 <davidgelmacorral@gmail.com>
2025-12-15 12:15:23 +0100

    Implementar splashScreen i crear LoginActivity,RegisterActivity

--- app/src/main/java/com/daviddam/projectecafeteriadavidgelmacorral2ndam/MainActivity.kt
+++ app/src/main/java/com/daviddam/projectecafeteriadavidgelmacorral2ndam/MainActivity.kt
@@ -3,18 +3,16 @@ package com.daviddam.projectecafeteriadavidgelmacorral2ndam
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
+        Thread.sleep(3000)
+        installSplashScreen()
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
